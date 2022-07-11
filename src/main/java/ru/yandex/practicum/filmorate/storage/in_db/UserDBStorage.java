e19eac8343adc7ade8ba082126a612209499595f
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-07-11 12:19:46 +0300

    feat: add obtaining user by id

--- src/main/java/ru/yandex/practicum/filmorate/storage/in_db/UserDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/in_db/UserDBStorage.java
@@ -1,13 +1,29 @@
 package ru.yandex.practicum.filmorate.storage.in_db;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.user.User;
 import ru.yandex.practicum.filmorate.storage.DataStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.List;
 
+@Slf4j
 @Component("userDBStorage")
 public class UserDBStorage implements DataStorage<User> {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDBStorage(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public User create(User baseEntity) {
         return null;
@@ -20,7 +36,12 @@ public class UserDBStorage implements DataStorage<User> {
 
     @Override
     public User get(long id) {
-        return null;
+        String sql = "SELECT * FROM users WHERE user_id = ?";
+        List<User> users = jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs), id);
+        if (users.size() < 1) {
+            throw new NotFoundException("User with id " + id + " isn't exist.");
+        }
+        return users.get(0);
     }
 
     @Override
@@ -41,5 +62,14 @@ public class UserDBStorage implements DataStorage<User> {
     @Override
     public User update(User baseEntity) {
         return null;
+    }
+
+    private User makeUser(ResultSet rs) throws SQLException {
+        long id = rs.getLong("user_id");
+        LocalDate birthday = rs.getDate("birthday").toLocalDate();
+        String email = rs.getString("email");
+        String login = rs.getString("login");
+        String name = rs.getString("name");
+        return new User(id, email, login, name, birthday);
     }
 }
